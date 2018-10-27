42f6b838dcb4b21e61ea587af7c7cd063d7afedd
CAwesome <caewerbring@gmail.com>
2018-10-27 17:32:54 +0200

    ADDED keydown press

--- main.js
+++ main.js
@@ -4,8 +4,5 @@ function lol(){
 
 
 $(document).keypress(function(e){
-    var checkWebkitandIE=(e.which==26 ? 1 : 0);
-    var checkMoz=(e.which==122 && e.ctrlKey ? 1 : 0);
-
-    if (checkWebkitandIE || checkMoz) $("body").append("<p>ctrl+z detected!</p>");
+      console.log("pressed: " + e.key)
 });
