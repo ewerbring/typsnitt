48d2cc1131012d0ff5f28d1ea3e3df93e201b3cc
CAwesome <caewerbring@gmail.com>
2018-10-27 17:25:25 +0200

    Added some lol stuff (great commit msg)

--- main.js
+++ main.js
@@ -2,3 +2,10 @@ function lol(){
     alert("TEST");   
 }
 
+
+$(document).keypress(function(e){
+    var checkWebkitandIE=(e.which==26 ? 1 : 0);
+    var checkMoz=(e.which==122 && e.ctrlKey ? 1 : 0);
+
+    if (checkWebkitandIE || checkMoz) $("body").append("<p>ctrl+z detected!</p>");
+});
