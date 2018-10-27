f5f27727b66da5d2ac2465fd83a53ffe4d65a6f5
CAwesome <caewerbring@gmail.com>
2018-10-27 18:21:37 +0200

    added working logic

--- main.js
+++ main.js
@@ -1,15 +1,49 @@
-function lol(){
-    alert("TEST");   
+function lol() {
+    for (var i = 0; i <= 20; i++) {
+        console.log(i);
+    }
 }
 
+var printedString = "asdasd";
 
-$(document).keypress(function(e){
+var fontSize = 40;
+
+$(document).keypress(function(e) {
     //when key is pressed, add new letter to site
     var container = document.querySelector(".coolio");
-    var newDiv = document.createElement("DIV"); 
-    newDiv.innerHTML = "lol";
-    newDiv.style.cssText = "display:inline; position:relative;";
+    var newDiv = document.createElement("DIV");
+    newDiv.innerHTML = e.key;
+    var topMargin = getHeightForPreviousString(printedString);
+    newDiv.style.cssText = "display:inline; position:relative;top:" + topMargin + "px;";
     container.appendChild(newDiv);
     //position said letter correctly
-      console.log("pressed: " + e.key);
+    console.log("pressed: " + e.key);
+    printedString = printedString + e.key;
 });
+
+function getHeightForPreviousString(previousString) {
+    //a angled up
+    //d straight right 
+    //c straight down
+    //l straight up
+    //r angled down
+
+    var totalHeight = 0;
+
+    for (var i = 0; i < previousString.length; i++) {
+        var thisChar = previousString.charAt(i)
+        var thisCharHeight = heightForCharacter(thisChar);
+        totalHeight = totalHeight + thisCharHeight;
+    }
+
+    return totalHeight;
+}
+
+function heightForCharacter(character) {
+    var height = 0;
+    if (character == 'a') {
+        height = -fontSize;
+    }
+
+    return height;
+}
