fd6dd9bb5c5e556e660fadd1600fa2629813d870
CAwesome <caewerbring@gmail.com>
2018-10-27 18:00:51 +0200

    Added dynamic additions of text, unpositioned but inline

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ function lol(){
 
 $(document).keypress(function(e){
     //when key is pressed, add new letter to site
+    var container = document.querySelector(".coolio");
+    var newDiv = document.createElement("DIV"); 
+    newDiv.innerHTML = "lol";
+    newDiv.style.cssText = "display:inline; position:relative;";
+    container.appendChild(newDiv);
     //position said letter correctly
-      console.log("pressed: " + e.key)
+      console.log("pressed: " + e.key);
 });
