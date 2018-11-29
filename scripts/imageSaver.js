d258d47301aae69aef27e8165d80b2591f9f45bd
CAwesome <caewerbring@gmail.com>
2018-11-29 10:09:02 +0800

    Working canvas

---
+++ scripts/imageSaver.js
@@ -0,0 +1,33 @@
+function SaveCanvas(){
+  // html2canvas($("#allContent"), {
+  //     onrendered: function(canvas) {
+  //       saveAs(canvas.toDataURL(), 'canvas.png');
+  //     }
+  //   });
+
+   html2canvas($("#allContent"), {
+      allowTaint: true,
+      onrendered: function(canvas) {
+        Canvas2Image.saveAsPNG(canvas);
+      }
+    });
+}
+
+ function saveAs(uri, filename) {
+    var link = document.createElement('a');
+    if (typeof link.download === 'string') {
+      link.href = uri;
+      link.download = filename;
+
+      //Firefox requires the link to be in the body
+      document.body.appendChild(link);
+
+      //simulate click
+      link.click();
+
+      //remove the link when done
+      document.body.removeChild(link);
+    } else {
+      window.open(uri);
+    }
+  }
